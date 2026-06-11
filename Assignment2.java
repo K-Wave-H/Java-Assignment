9ded6d91812d371b4fb14d3d7b90ce2cb5fef20a
K-Wave-H <henryagyare15@gmail.com>
2026-06-11 03:06:54 +0000

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,25 @@
+public class Assignment2 {
+    public static void main(String[] args ){
+        System.out.println(" x                                                            x");
+        System.out.println(" x                                                            x");
+        System.out.println(" x                                                            x");
+        System.out.println(" x                                                            x");
+        System.out.println(" xxxxxxxxxxxx       xxxxxxxxxxxxxxxxxxxx           xxxxxxxxxxxx");
+        System.out.println(" x          x       x                  x           x          x");
+        System.out.println(" x          x       x                  x           x          x");
+        System.out.println(" x          x       x                  x           x          x");
+        System.out.println(" x          x       x                  x           x          x");
+        System.out.println();
+
+        System.out.println("          T           ");
+        System.out.println("         TTT          ");
+        System.out.println("        TTTTT          ");
+        System.out.println("       TTTTTTT         ");
+        System.out.println("      TTTTTTTTT        ");
+        System.out.println("     TTTTTTTTTTT       ");
+        System.out.println("    TTTTTTTTTTTTT      ");
+        System.out.println("   TTTTTTTTTTTTTTT     ");
+        System.out.println("  TTTTTTTTTTTTTTTTT    ");
+        System.out.println(" TTTTTTTTTTTTTTTTTTT   ");
+    }
+}
